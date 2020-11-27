5f46cf7290279c9307908e5637eda3b193c0e68f
Ariel Xu <arielxu@live.com>
2020-11-27 15:52:41 +0800

    fixed the xml and wxss for the list page and uncommented the global userId

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 App({
   onLaunch: function () {
-    // const host = this.globalData.host
-    //   let page = this
-    // console.log("beginning login")
-    // wx.login({
-    //   success: (res) => {
-    //     console.log(res)
-    //     // insert next code here
-    //     wx.request({
-    //       url: host + "login",
-    //       method: "post",
-    //       data: {
-    //         code: res.code
-    //       },
-    //       // insert next code here
-    //       success: (res) => {
-    //         console.log('logged in', res.data)
-    //         this.globalData.userId = res.data.user.id
-    //       }
-    //     })
-    //   }
-    // })
+    const host = this.globalData.host
+      let page = this
+    console.log("beginning login")
+    wx.login({
+      success: (res) => {
+        console.log(res)
+        // insert next code here
+        wx.request({
+          url: host + "login",
+          method: "post",
+          data: {
+            code: res.code
+          },
+          // insert next code here
+          success: (res) => {
+            console.log('logged in', res.data)
+            this.globalData.userId = res.data.user.id
+          }
+        })
+      }
+    })
   },
   globalData: {
     host: 'http://localhost:3000/',
